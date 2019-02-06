6b2de669d3d5a5673f58cfdaf67919404e70db5b
andres <carlos.cuervo@correounivalle.edu.co>
2019-02-06 09:13:48 -0500

    set route account

--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const accountController=require('../controllers/accountController'); 
 
 router.get('/',accountController.getAccounts); //OBTIENE TODOS LOS USUARIOS
-router.get('/:id',accountController.getEspecifyAccount); //OBTIENE UN USUARIO ESPECIFICO
+//router.get('/:id',accountController.getEspecifyAccount); //OBTIENE UN USUARIO ESPECIFICO
 router.get('/:id',accountController.getAccountByUser); //OBTIENE UNA CUENTA DE UN USUARIO ESPECIFICO
 router.post('/',accountController.createAccount);//CREA UN USUARIO
 router.put('/:id',accountController.updateAccount);//ACTUALIZA UN USUARIO

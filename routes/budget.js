3a266cdc37dcd16da43e82f161f921e58f0c9d13
andres <carlos.cuervo@correounivalle.edu.co>
2019-02-06 09:23:37 -0500

    set routes by specify user id

--- routes/budget.js
+++ routes/budget.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const budgetController=require('../controllers/budgetController'); 
 
 router.get('/',budgetController.getBudgets); //OBTIENE TODOS LOS USUARIOS
-router.get('/:id',budgetController.getEspecifyBudget); //OBTIENE UN USUARIO ESPECIFICO
+//router.get('/:id',budgetController.getEspecifyBudget); //OBTIENE UN USUARIO ESPECIFICO
 router.get('/:id',budgetController.getBudgetByUser); //OBTIENE UN PRESUPUESTO ASOCIADO A UN USUARIO ESPECIFICO
 router.post('/',budgetController.createBudget);//CREA UN USUARIO
 router.put('/:id',budgetController.updateBudget);//ACTUALIZA UN USUARIO
--- routes/item.js
+++ routes/item.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const itemController=require('../controllers/itemController'); 
 
 router.get('/',itemController.getItems); //OBTIENE TODOS LOS USUARIOS
-router.get('/:id',itemController.getEspecifyItem); //OBTIENE UN USUARIO ESPECIFICO
+//router.get('/:id',itemController.getEspecifyItem); //OBTIENE UN USUARIO ESPECIFICO
 router.get('/:id',itemController.getItemByUser); //OBTIENE UN USUARIO ESPECIFICO
 router.post('/',itemController.createItem);//CREA UN USUARIO
 router.put('/:id',itemController.updateItem);//ACTUALIZA UN USUARIO
